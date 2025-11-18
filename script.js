5f509102321a0d6dc84594b1360a5d8ec38046b9
noha-kasmi <nohakasmi02@gmail.com>
2025-11-18 23:55:40 +0100

    le grid bien placer avec l'images pour former des rooms

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-const burgerIcon = document.getElementById('burger-icon');
-const sidebar = document.getElementById('sidebar');
-
-burgerIcon.addEventListener('click', function() {
-    this.classList.toggle('active');
-    sidebar.classList.toggle('active');
-});
-
-document.addEventListener('click', function(event) {
-    if (!sidebar.contains(event.target) && !burgerIcon.contains(event.target) && sidebar.classList.contains('active')) {
-        burgerIcon.classList.remove('active');
-        sidebar.classList.remove('active');
-    }
-});
 
 
